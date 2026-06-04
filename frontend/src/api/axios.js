f1c925a905213bc059e1b3edc98beac5caf997a9
praful-koli <prafulkoli852@gmail.com>
2026-06-04 11:06:12 +0530

    Update axios instance base URL and refresh token endpoint to match new deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://kodex-debug-battel-5.onrender.com/api',
+  baseURL: 'https://kodex-debug-battel-5-jk2bhlb0z-praful-s-projects-220d4c44.vercel.app/api',
   withCredentials: true, // Required to send cookies
 });
 
@@ -31,7 +31,7 @@ instance.interceptors.response.use(
       
       try {
         // Attempt to refresh token
-        const res = await instance.post('https://kodex-debug-battel-5.onrender.com/api/auth/refresh', {}, { withCredentials: true });
+        const res = await instance.post('https://kodex-debug-battel-5-jk2bhlb0z-praful-s-projects-220d4c44.vercel.app/api/auth/refresh', {}, { withCredentials: true });
         
         // Save new access token
         localStorage.setItem('token', res.data.accessToken);
