440aeb1035a6aeef97e1691e7c1cbd05815922ae
praful-koli <prafulkoli852@gmail.com>
2026-06-04 11:26:41 +0530

    Update axios instance base URL to use environment variable for consistency

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://kodex-debug-battel-5.onrender.com/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true, // Required to send cookies
 });
 
@@ -31,7 +31,7 @@ instance.interceptors.response.use(
       
       try {
         // Attempt to refresh token
-        const res = await instance.post('https://kodex-debug-battel-5.onrender.com/api/auth/refresh', {}, { withCredentials: true });
+        const res = await instance.post(`${import.meta.env.VITE_API_URL}/api/auth/refresh`, {}, { withCredentials: true });
         
         // Save new access token
         localStorage.setItem('token', res.data.accessToken);
