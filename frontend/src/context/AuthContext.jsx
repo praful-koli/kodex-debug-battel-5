5f428df0ee19c0b4bcb59d95e42900c46dea31a0
praful-koli <prafulkoli852@gmail.com>
2026-06-04 11:14:19 +0530

    Update axios instance base URL and refactor import in AuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState, useEffect } from 'react';
-import axios from '../api/axios';
+import  { createContext, useState, useEffect } from 'react';
+
 import instance from '../api/axios';
 
 export const AuthContext = createContext();
